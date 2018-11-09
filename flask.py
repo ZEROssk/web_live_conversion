ab422a6280f08b01c2df864d6a3a21150b60f6b6
zerossk <a.k.jack.ssk@gmail.com>
2018-11-09 11:40:17 +0900

    firast commit

---
+++ flask.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, 
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def maine():
+    return render_template("maine.html")
+
+if __naem__ == "_main__":
+    app.run(debug=True)
+
