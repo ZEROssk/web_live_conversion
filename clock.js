a8a6f92c975d962fdfbc15aab1fe0d09c120bb20
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2018-11-09 16:08:36 +0900

    update

---
+++ clock.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function set2fig(num) {
+	let ret;
+	if( num < 10 ) { ret = "0" + num; }
+	else { ret = num; }
+	
+	return ret;
+}
+function showClock2() {
+	let nowTime = new Date();
+	let nowHour = set2fig( nowTime.getHours() );
+	let nowMin  = set2fig( nowTime.getMinutes() );
+	let nowSec  = set2fig( nowTime.getSeconds() );
+	let msg = "Time: " + nowHour + ":" + nowMin + ":" + nowSec;
+	document.getElementById("RealtimeClockArea2").innerHTML = msg;
+}
+setInterval('showClock2()',1000);
+
