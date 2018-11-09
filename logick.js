389d504d38bc93949d507f038192161803c1a1bf
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2018-11-09 16:35:24 +0900

    update

---
+++ logick.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function addValue(){
+	let idname = "input";
+	s = document.getElementById(idname).value;
+	let pvname = idname + "pv";
+	document.getElementByIdpvname).innerHTML = s;
+}
