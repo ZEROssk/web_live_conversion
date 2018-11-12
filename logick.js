2c1227d10bf1a711f44ddc2f05c1e6e9b48bb248
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2018-11-12 15:48:47 +0900

    ひらがな非対応-version

--- logick.js
+++ logick.js
@@ -1,6 +1,20 @@
-function addValue(){
+//'use strict'
+
+function addValue()
+{
 	let input = 'input'; 
 	let output = 'output';
 	let s = document.getElementById(input).value;
-	document.getElementById(output).value = s;
+	
+	function hex(s) {
+		console.log(s)
+		var result = "";
+		for(var i=0; i<s.length; ++i){
+			var h = ("0"+s.charCodeAt(i).toString(16)).substr(-2);
+			result += h;
+		}
+		return result;
+	}
+	document.getElementById(output).value = hex(s);
 }
+
