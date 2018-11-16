b0450b0b7af7aea2fa2c9b42a66f5d73f7ea60ec
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2018-11-16 23:44:45 +0900

    clockのid変更

--- clock.js
+++ clock.js
@@ -7,13 +7,15 @@ function set2fig(num) {
 	
 	return ret;
 }
+
 function showClock2() {
 	let nowTime = new Date();
 	let nowHour = set2fig( nowTime.getHours() );
 	let nowMin  = set2fig( nowTime.getMinutes() );
 	let nowSec  = set2fig( nowTime.getSeconds() );
 	let msg = "Time: " + nowHour + ":" + nowMin + ":" + nowSec;
-	document.getElementById("RealtimeClockArea2").innerHTML = msg;
+	document.getElementById("RealtimeClockArea").innerHTML = msg;
 }
+
 setInterval('showClock2()',1000);
 
